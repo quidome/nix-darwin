54cca2f185f69f54b3d199b5af7749419f78dddf
Quido Meijer <qmeijer@bol.com>
2025-06-05 10:26:15 +0200

    Don't show startup tips

--- home-manager/programs/zellij/default.nix
+++ home-manager/programs/zellij/default.nix
@@ -21,6 +21,7 @@ in
         scrollback_editor = lib.mkIf helixEnabled (lib.getExe pkgs.helix);
         pane_frames = false;
         default_layout = "layout";
+        show_startup_tips = false;
       };
     };
 
