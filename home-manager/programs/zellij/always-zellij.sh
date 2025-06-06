b4b8aa1e8bf3027eb257062525884ea5e4bf1f4c
Quido Meijer <qmeijer@bol.com>
2025-06-06 10:22:50 +0200

    clean up script

--- home-manager/programs/zellij/always-zellij.sh
+++ home-manager/programs/zellij/always-zellij.sh
@@ -1,7 +1,6 @@
 # some exceptions in which case zellij should not be used
 if  [ -n "$TMUX" ] ||
     [[ "$GIO_LAUNCHED_DESKTOP_FILE" == *"guake.desktop"* ]] ||
-    [ "$__CFBundleIdentifier" = "io.lapce" ] ||
     [[ "$(tty)" =~ /dev/tty[0-9] ]] ||
     [[ "$TERM" == screen* ]] ||
     [[ "$TERM" == "" ]] ||
@@ -14,11 +13,10 @@ then
     return 1
 fi
 
-
 if [[ $TERM != "screen-256color" && $TERM != "linux" && -z "$ZELLIJ" ]] ; then
-if [[ "$(zellij ls -s)" = "default-session" ]]; then
-    zellij attach 'default-session'
-else
-    zellij attach -c 'default-session'
-fi
+    if [[ "$(zellij ls -s)" = "default-session" ]]; then
+        zellij attach 'default-session'
+    else
+        zellij attach -c 'default-session'
+    fi
 fi
